7d829e4f95a82ed0ced429563c93b64ce84154eb
saniyar-dev <saniyar.dev@gmail.com>
2025-07-26 21:39:32 +0330

    [ INIT ]

---
+++ config/site.ts
@@ -0,0 +1,28 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "کمک",
+      href: "/help-feedback",
+    },
+    {
+      label: "خروج",
+      href: "/logout",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "کمک",
+      href: "/help-feedback",
+    },
+    {
+      label: "خروج",
+      href: "/logout",
+    },
+  ],
+  links: {
+  },
+};
