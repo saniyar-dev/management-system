010a93d69513aff2ce92f5d3bb423620f9127281
saniyar-dev <saniyar.dev@gmail.com>
2025-08-04 13:18:15 +0330

    [ ADD ] jobs component and sectionized things

--- app/dashboard/clients/addClient.tsx
+++ app/dashboard/clients/addClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { Card, CardFooter, Image } from "@heroui/react";
 import {
   Modal,
   ModalContent,
@@ -11,10 +12,9 @@ import {
   useDisclosure,
   useDraggable,
   Input,
-  Link,
-  Checkbox,
 } from "@heroui/react";
 import { PlusIcon, MailIcon, LockIcon } from "@/components/icons";
+import JobsComponent from "./jobs";
 
 export function AddClientComponent() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -38,57 +38,49 @@ export function AddClientComponent() {
       <Button color="primary" endContent={<PlusIcon />} onPress={onOpen}>
         ایجاد مشتری جدید
       </Button>
-      <Modal ref={targetRef} isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal
+        ref={targetRef}
+        isOpen={isOpen}
+        size="5xl"
+        onOpenChange={onOpenChange}
+      >
         <ModalContent>
           {(onClose) => (
             <>
               <ModalHeader {...moveProps} className="flex flex-col gap-1">
                 ایجاد مشتری جدید
               </ModalHeader>
-              <form action={submit}>
-                <ModalBody>
-                  <Input
-                    endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none shrink-0" />
-                    }
-                    label="نام و نام خاوادگی"
-                    id="name"
-                    name="name"
-                    placeholder="نام مشتری"
-                    variant="bordered"
-                  />
-                  <Input
-                    endContent={
-                      <LockIcon className="text-2xl text-default-400 pointer-events-none shrink-0" />
-                    }
-                    label="شماره موبایل"
-                    id="phone"
-                    name="phone"
-                    placeholder="09900790244"
-                    variant="bordered"
-                  />
-                  <div className="flex py-2 px-1 justify-between">
-                    <Checkbox
-                      classNames={{
-                        label: "text-small",
-                      }}
-                    >
-                      Remember me
-                    </Checkbox>
-                    <Link color="primary" href="#" size="sm">
-                      Forgot password?
-                    </Link>
-                  </div>
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="flat" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button color="primary" type="submit">
-                    Sign in
-                  </Button>
-                </ModalFooter>
-              </form>
+              <section className="flex justify-between py-6">
+                <form action={submit} className="w-xl">
+                  <ModalBody>
+                    <Input
+                      label="نام و نام خاوادگی"
+                      id="name"
+                      name="name"
+                      placeholder="نام مشتری"
+                      variant="bordered"
+                    />
+                    <Input
+                      label="شماره موبایل"
+                      id="phone"
+                      name="phone"
+                      placeholder="09900790244"
+                      variant="bordered"
+                    />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="flat" onPress={onClose}>
+                      بستن
+                    </Button>
+                    <Button color="primary" type="submit">
+                      ثبت
+                    </Button>
+                  </ModalFooter>
+                </form>
+                <section className="flex flex-col px-6">
+                  <JobsComponent />
+                </section>
+              </section>
             </>
           )}
         </ModalContent>
