b059457d45cd8d06d0196b213503bff52f4ef872
saniyar-dev <saniyar.dev@gmail.com>
2025-08-05 14:14:54 +0330

    [ FIX ] z-index of loading component to overlay the modal too

--- components/loading.tsx
+++ components/loading.tsx
@@ -5,7 +5,7 @@ import { Spinner } from "@heroui/spinner";
 export default function Loading({ pending }: { pending: boolean }) {
   if (pending) {
     return (
-      <div className="w-screen h-screen flex items-center justify-center z-50 fixed bottom-0 right-0 backdrop-blur-xs bg-white/10">
+      <div className="w-screen h-screen flex items-center justify-center z-[1000] fixed bottom-0 right-0 backdrop-blur-xs bg-white/10">
         <Spinner size="lg" />
       </div>
     );
